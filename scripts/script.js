7e262ff9e21efa844ade96fece62cb07c6dcee6a
nivedmundiyath <102532951+nivedmundiyath@users.noreply.github.com>
2022-05-14 21:17:29 +0530

    final commit

--- scripts/script.js
+++ scripts/script.js
@@ -52,44 +52,123 @@ var tdl_Table = (apiRes) => {
 
 
         var testvar = document.getElementById("tdlTable");
-        var response = JSON.parse(apiRes)
+        var response = JSON.parse(apiRes);
         console.log("API response :", response);
 
         var column = `<tr>
                         <th>Userid</th>
+                        <th>id</th>
                         <th>Title</th>
                         <th>Completed</th>
-                        <th>Checkbox</th>
                       </tr>` ;
         testvar.innerHTML = column;
                        
-        for (var value=0; value < response.length; value++){
+        for (var value=0; value < response.length; value++)
+        {
 
-                var row = `<tr>
-                                <td>${response[value].id}</td>
-                                <td>${response[value].title}</td>
-                                <td>${response[value].completed}</td>
-                                <td> <input type="checkbox" id="checkbox" onclick="validateCheckbox();"> </td>
-                           </tr>` ;
+                var tr = document.createElement("tr");
+                var header_data = Object.keys(response[value]);
+                // console.log(header_data);
+                header_data.forEach((key) => {
+                        var td = document.createElement("td");
+                        var key_new = key.replace(/["']/g, "");
+                        // console.log("input:", key_new);
 
-                testvar.innerHTML+= row;        
-                
+                        if (key_new == "completed")
+                        {
+                                var input=document.createElement("input");
+                                input.setAttribute("type","checkbox");
+                                input.setAttribute("id","checkboxComp");
+                                // input.setAttribute("onchange","validateCheckbox();");
 
+
+
+                                if(response[value][key]) 
+                                {
+                                // console.log("input all true:", input);
+                                input.setAttribute("checked","true");
+                                input.setAttribute("disabled","true");
+                                // console.log("TD all true", td);
+
+                                       }
+                                input.addEventListener("change", (event)=>{
+                                        if(event.currentTarget.checked){
+                                                count++;
+                                                validateCheckboxtest();
+                                        }
+                                        else{
+                                                count--;
+                                        }
+                                })
+                                       td.appendChild(input);
+
+                        // else{
+                        //         // console.log("input all false", input);
+                        //         td.appendChild(input);
+                        //         // console.log("TD all false", td);
+
+                        // }
+                        }
+                        else
+                                
+                                td.innerHTML = response[value][key];
+
+                        
+                tr.appendChild(td);
+
+                })
+                testvar.appendChild(tr);
         }
+                // var row = `<tr>
+                //                 <td>${response[value].id}</td>
+                //                 <td>${response[value].title}</td>
+                //                 <td>${response[value].completed}</td>
+                //                 <td> <input type="checkbox" id="checkbox" onclick="validateCheckbox();"> </td>
+                //            </tr>` ;
 
+                // testvar.innerHTML+= row;        
+                
 
 
 }
 var count =0;
-var validateCheckbox = () => {
+var c_status = false;
 
+var validateCheckboxtest = () => {
 
-        count+=1;
+        // var checkbox = document.querySelectorAll("#checkboxComp");
 
-        if (count == 5)
-              {                                       
-                alert("Congrats ! 5 task completed");
-}
+        let promise = new Promise(function(resolve,reject){
+                if(count == 5) {
+                        resolve("Congrats ! 5 task completed");
+                }
+                else if(count>5)
+                {
+                        count =0;
+                        reject();
+                }
+        })
+        promise.then(function(s){
+                alert(s);
+
+        })
+        .catch(function() {
+                window.location.href="todolist.html";
+        })
+        
+
+        // if (checkbox.checked == true)
+        //         count+=1;
+        // else
+        //         count-=1;
+
+        // if (count == 5)
+        //         {                                       
+        //         alert("Congrats ! 5 task completed");
+        //         }
+        //         console.log("count", count );
+
+        
 
 
         
@@ -97,3 +176,14 @@ var validateCheckbox = () => {
 
 
 }
+
+// var checkbox = document.querySelectorAll("#checkboxComp");
+
+
+// checkbox.addEventListener('change', (event) => {
+//         if (event.currentTarget.checked) {
+//           alert('checked');
+//         } else {
+//           alert('not checked');
+//         }
+//       })
